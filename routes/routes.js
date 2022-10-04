513ac69cc2288324e2610a3bf39d5da2b9ad90e0
jpbazotti <62719223+jpbazotti@users.noreply.github.com>
2022-10-04 00:33:15 -0300

    added cors

--- routes/routes.js
+++ routes/routes.js
@@ -6,19 +6,47 @@ const controllerSection = require('../controllers/controllerSection');
 const controllerGuitar = require('../controllers/controllerGuitar');
 const controllerCart = require('../controllers/controllerCart');
 const controllerOrder = require('../controllers/controllerOrder');
-//login routes
+//LOGIN routes
+//vou colocar o que precisa no corpo/query de cada request
+
+//Faz login
+//req.body.email
+//req.body.password
 router.post('/login', controllerLogin.login);
+
+//Faz registro
+//req.body.name
+//req.body.passwprd
+//req.body.cpf
+//req.body.email
+//req.body.adress
+//req.body.admin->bool
 router.post('/register', controllerLogin.registerUser);
-//part routes
-router.get('/getPartsOfType', controllerPart.getPartofType);
-router.get('/getAllSections', controllerSection.getSections);
+
+//retorn todos os usuarios
 router.get('/all', controllerLogin.allUsers);
+
+
+//PART routes
+
+//pega todas as partes da sessao section
+//req.query.section
+router.get('/getPartsOfType', controllerPart.getPartofType);
+
+
+//Registra nova parte
+//req.body.name,
+//req.body.quantity,
+//req.body.section,
+//req.body.price,
+//req.body.description
 router.post('/registerPart', controllerLogin.isAdmin,controllerPart.registerPart);
 router.put('/updateQuant', controllerLogin.isAdmin,controllerPart.updateQuantity);
 router.delete('/deletePart', controllerLogin.isAdmin,controllerPart.deletePart);
 router.get('/getPartsByIDList',controllerPart.findPartsbyidList);
 //section route, devolopment only
 router.post('/registerSection', controllerSection.registerSection);
+router.get('/getAllSections', controllerSection.getSections);
 //guitar routes
 router.get("/getGuitars",controllerGuitar.getAllGuitars);
 router.post("/addGuitar",controllerGuitar.registerGuitar);
